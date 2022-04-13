03727600011fb3b343ae2e32140e0100aecef7f4
habeebgit <habeeb_mohammed@hotmail.com>
2022-04-13 15:13:21 -0500

    React Redux -Saga code

--- src/App.js
+++ src/App.js
@@ -1,4 +1,28 @@
-import logo from './logo.svg';
+import React from 'react'
+
+// bring Provider from react-redux, it's the bridge for connecting react to redux
+import { Provider } from 'react-redux'
+
+// Bring the redux store too
+import store from './redux/store'
+
+// Components
+import Todo from './components/Todo'
+
+const App = () => {
+  return (
+    // Register your redux Provider here
+    <Provider store={store}>
+      <Todo />
+    </Provider>
+  )
+}
+export default App
+
+
+
+
+/* import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -23,3 +47,4 @@ function App() {
 }
 
 export default App;
+ */
